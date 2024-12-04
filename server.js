3a0873d02041f31c8317286a916e38339310aab2
Soltobekov Amin <1kkro123@gmail.com>
2024-12-05 01:41:06 +0600

    fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // Подключаем cors
+const cors = require("cors");
 
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
